466a0b778cc482d84ef5365d142c310e08694539
Wildan A <115684223+comzfaza@users.noreply.github.com>
2023-02-02 11:56:36 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<link rel="stylesheet" href="login.css" />
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    
+</head>
+<body>
+    <h1>Login</h1>
+    <div>
+   <form action="login">
+        <label for="username">username :</label>
+        <input type="text" name="username" id="username">
+        <br><br>
+        <label for="password">password :</label>
+        <input type="password" name="password" id="password">
+        <br><br>
+        <button>enter</button>
+    </form>
+   </div>
+</body>
+</html>
